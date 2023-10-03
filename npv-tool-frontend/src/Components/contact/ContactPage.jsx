0eb368e2cf982ba391d444780fc5a14b553c03ad
getinet amare <getinetamare@gmail.com>
2023-10-03 15:51:47 +0300

    add preloader

--- npv-tool-frontend/src/Components/contact/ContactPage.jsx
+++ npv-tool-frontend/src/Components/contact/ContactPage.jsx
@@ -1,9 +1,72 @@
 const ContactPage = () => {
-    return (<div>
-      <h1>
-      <p>Radical: &#x221A; or &#8730;</p>
-      <p>Radical: &#x221A;(x + y)</p>
-      </h1>Contact page</div>);
+  const handleContact = (e) => {
+    e.preventDefault();
+    alert("redirect to email for sending");
   };
-  export default ContactPage;
-  
+  return (
+    <div className="p-4">
+      <form
+        onSubmit={handleContact}
+        class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md m-4"
+      >
+        <div class="mb-4">
+          <label
+            for="name"
+            class="block mb-2 text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            required
+            id="name"
+            name="name"
+            class="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-green-500 focus:border-green-500"
+          />
+        </div>
+        <div class="mb-4">
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            required
+            id="email"
+            name="email"
+            class="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-green-500 focus:border-green-500"
+          />
+        </div>
+        <div class="mb-4">
+          <label
+            for="message"
+            class="block mb-2 text-sm font-medium text-gray-700"
+          >
+            Message
+          </label>
+          <textarea
+            required
+            id="message"
+            name="message"
+            rows="4"
+            class="w-full px-3 py-2 placeholder-gray-400 border rounded-md focus:outline-none focus:ring-green-500 focus:border-green-500"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          class="px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600"
+        >
+          Submit
+        </button>
+        {/* <h1>
+          <p>Radical: &#x221A; or &#8730;</p>
+          <p>Radical: &#x221A;(x + y)</p>
+        </h1> */}
+        {/* Contact page */}
+      </form>
+    </div>
+  );
+};
+export default ContactPage;
--- npv-tool-frontend/src/Components/home/HomePage.jsx
+++ npv-tool-frontend/src/Components/home/HomePage.jsx
@@ -1,11 +1,12 @@
-import {  NavLink } from "react-router-dom";
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 import classNames from "classnames";
 import SpecificEligibilityLoanData from "../data/NPVScenarioLists.json";
-
+import { FaSpinner } from "react-icons/fa";
 const HomePage = () => {
   const [cardBackgroundColors, setCardBackgroundColors] = useState({});
-
+  const [nPVScenarioLists, setNPVScenarioLists] = useState([]);
+  const [nPVScenarioListsError, setNPVScenarioListsError] = useState([]);
   const handleCardMouseEnter = (index) => {
     const colors = [
       "bg-red-500",
@@ -16,43 +17,70 @@ const HomePage = () => {
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     setCardBackgroundColors({ [index]: randomColor });
   };
+  useEffect(() => {
+    setNPVScenarioListsError(true);
+    const timeoutId = setTimeout(() => {
+      setNPVScenarioLists(
+        SpecificEligibilityLoanData.npv_scenarios.map(
+          (npvScenario) => npvScenario
+        )
+      );
+      setNPVScenarioListsError(false);
+    }, 1000);
+    return () => clearTimeout(timeoutId);
+  }, []);
   return (
     <div
       className={`mx-w-[1240px] mx-auto grid md:grid-cols-3 gap-8 ease-in-out bg-gray-100`}
     >
-      {SpecificEligibilityLoanData.npv_scenarios.map((npvScenario) => (
-        <div key={npvScenario.id}>
-          <div
-            className={`${classNames(
-              "p-4",
-              "rounded",
-              "cursor-pointer",
-              cardBackgroundColors[npvScenario.id + 1]
-            )}w-full border-2 shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 bg-gray-200`}
-            onMouseEnter={() => handleCardMouseEnter(npvScenario.id)}
-          >
-            <header className="text-2xl font-bold text-center py-1 bg-[#00563F] text-white grounded-l">
-              <NavLink
-                to={`/${npvScenario.id}`}
-                className={({ isActive }) =>
-                  isActive ? "underline text-green-200 font-bold" : ""
-                }
-              >
-                {npvScenario.name}
-              </NavLink>
-            </header>
-            <p>
-              <hr />
-            </p>
-            <article>
-              <header>
-                <h2 className="px-4 font-semibold">
-                  About {npvScenario.name} NPV
-                </h2>
+      {nPVScenarioListsError ? (
+        <div className="p-8 m-5">
+          <span className="flex justify-center">
+            <FaSpinner
+              className="animate-spin rounded-full h-6 w-6 text-green-600"
+              fontSize="large"
+            />
+            <p className="text-green-600 font-bold"> Loading...</p>
+          </span>
+        </div>
+      ) : (
+        !nPVScenarioListsError &&
+        nPVScenarioLists.map((npvScenario) => (
+          <div key={npvScenario.id}>
+            <div
+              className={`${classNames(
+                "p-4",
+                "rounded",
+                "cursor-pointer",
+                cardBackgroundColors[npvScenario.id + 1]
+              )}w-full border-2 shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 bg-gray-200`}
+              onMouseEnter={() => handleCardMouseEnter(npvScenario.id)}
+            >
+              <header className="text-2xl font-bold text-center py-1 bg-[#00563F] text-white grounded-l">
+                <NavLink
+                  to={`/${npvScenario.id}`}
+                  className={({ isActive }) =>
+                    isActive ? "underline text-green-200 font-bold" : ""
+                  }
+                >
+                  {npvScenario.name}
+                </NavLink>
               </header>
-              <p>{npvScenario.description}</p>
-              <ul>
-                {/* {npvScenario.group.map((productGroup, index) => (
+              <p>
+                <hr />
+              </p>
+              <article>
+                <header>
+                  <NavLink
+                    to={`/${npvScenario.id}`}
+                    className="px-4 font-bold text-green-950 hover:uppercase hover:cursor-pointer"
+                  >
+                    About {npvScenario.name} NPV
+                  </NavLink>
+                </header>
+                <p>{npvScenario.description}</p>
+                <ul>
+                  {/* {npvScenario.group.map((productGroup, index) => (
                   <>
                     <li className="p-2" key={productGroup.id}>
                       <Link
@@ -66,11 +94,12 @@ const HomePage = () => {
                     </li>
                   </>
                 ))} */}
-              </ul>
-            </article>
+                </ul>
+              </article>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
